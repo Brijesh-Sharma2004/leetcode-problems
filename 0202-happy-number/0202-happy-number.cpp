9223eb98bb2398e3349ea2f77e5bef33f209b371
Brijesh-Sharma2004 <sharmabrijesh97397@gmail.com>
2026-07-02 17:54:16 +0530

    Time: 0 ms (100%), Space: 8.3 MB (38.6%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int sumofsquares(int n) {
+    int sum = 0;
+    while(n > 0) {
+        int digit = n % 10;
+        sum = sum + digit*digit;
+        n = n / 10;
+    }
+    return sum;
+}
+
+    bool isHappy(int n) {
+        unordered_set<int> st;
+        
+        while(n != 1) {
+        if(st.find(n) != st.end()) {
+            return false;
+        }
+        st.insert(n);
+        n = sumofsquares(n);
+        }
+        return true;
+    }
+};
